b6a38e8b0aa57daf6ef3ff0890a8c8c3ff1e8b3a
Almeida Junior <luiz@jornadatecnologia.com>
2018-10-27 00:53:43 -0300

    Divindo papeis, passa ser agora dever do firebase gravar o id.

--- app/src/main/java/com/pucpr/puctcc/model/User.java
+++ app/src/main/java/com/pucpr/puctcc/model/User.java
@@ -1,19 +1,11 @@
 package com.pucpr.puctcc.model;
 
 public class User {
-    private Long id;
     private String name;
     private String email;
     private String password;
     private String typeUser;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
